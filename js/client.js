4a35c23ac030c6f3a800f9028badb5f84c41662d
lean <7764115+byog@users.noreply.github.com>
2020-05-14 18:44:14 +0800

    refactor(js/*): rewrite all js script in js folder
    refactor script step by step

--- js/client.js
+++ js/client.js
@@ -1,70 +1,63 @@
 C.client = (function () {
-
     var client = {
-
-        isTouch: ('ontouchstart' in window),
+        isTouch: 'ontouchstart' in window,
 
         init: function () {
-
-            C.log('Client: init');
+            C.log('Client: init')
 
             if (!this.isTouch) {
-
-                this.width = 320;
-                this.height = 548;
-                $(document.body).addClass('desktop');
-
+                this.width = 320
+                this.height = 548
+                document.body.classList.add('desktop')
             }
 
-            this.update();
-            $(window).resize(function () {
-                C.client.update();
-            });
+            this.update()
+
+            window.addEventListener('resize', function () {
+                C.client.update()
+            })
 
             // compatibility
-            // only supports browsers with CSS 3D Transforms
+            // only supports browsers with css 3D Transforms
             // Chrome, FF, IE10+
 
-            var s = document.body.style;
+            var s = document.body.style
 
-            C.client.isWebkit = 'webkitTransform' in s;
-            
+            C.client.isWebkit = 'webkitTransform' in s
+
             C.client.transformProperty =
-                'webkitTransform' in s ? 'webkitTransform' :
-                'mozTransform' in s ? 'mozTransform' :
-                'msTransform' in s ? 'msTransform' : 'transform';
+                'webKitTransform' in s
+                    ? 'webkitTransform'
+                    : 'mozTransform' in s
+                    ? 'mozTransform'
+                    : 'msTransform' in s
+                    ? 'msTransform'
+                    : 'transform'
 
-            var t = C.client.transformProperty;
+            var t = C.client.transformProperty
             C.client.transitionEndEvent =
-                t === 'webkitTransform' ? 'webkitTransitionEnd' : 'transitionend';
-
+                t === 'webkitTransform'
+                    ? 'webkitTransitionEnd'
+                    : 'transitionend'
         },
 
         update: function () {
-
             if (this.isTouch) {
-
-                this.width = window.innerWidth,
-                this.height = window.innerHeight;
+                ;(this.width = window.innerWidth),
+                    (this.height = window.innerHeight)
                 if (C.currentView) {
-                    C.currentView.updateBounds();
-                    C.currentView.onDragEnd(0);
+                    C.currentView.updateBounds()
+                    C.currentView.onDragEnd(0)
                 }
-
             } else {
-
-                var wrapper = C.$wrapper[0];
-                this.top = wrapper.offsetTop;
-                this.left = wrapper.offsetLeft;
-                this.right = this.left + this.width;
-                this.bottom = this.top + this.height;
-
+                var wrapper = C.$wrapper
+                this.top = wrapper.offsetTop
+                this.left = wrapper.offsetLeft
+                this.right = this.left + this.width
+                this.bottom = this.top + this.height
             }
+        },
+    }
 
-        }
-
-    };
-
-    return client;
-
-}());
+    return client
+})()
--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 var C = {
-
-    debug: window.location.hash.replace('#','') === 'debug',
+    debug: window.location.hash.replace('#', '') === 'debug',
 
     // dom elements
-    $wrapper: $('#wrapper'),
-    $log: $('#log'),
+    $wrapper: document.getElementById('wrapper'),
 
     // view states
     states: {
         LIST_COLLECTION_VIEW: 'lists',
-        TODO_COLLECTION_VIEW: 'todos'
+        TODO_COLLECTION_VIEW: 'todos',
     },
 
     isEditing: false,
@@ -17,108 +15,57 @@ var C = {
     ITEM_HEIGHT: 62,
 
     init: function () {
-
-        C.start = Date.now();
+        C.start = Date.now()
 
         // init some components
-        C.client.init();
-        C.db.init(C.debug);
-        C.touch.init();
-        C.listCollection.init();
+        C.client.init()
+        C.db.init(C.debug)
+        C.touch.init()
+        C.listCollection.init()
 
-        // restore state
+        // restore state 
         var data = C.db.data,
             state = data.state,
             lists = data.items,
-            i = lists.length;
+            i = lists.length
 
         switch (state.view) {
-
             case C.states.LIST_COLLECTION_VIEW:
-                C.log('App: init at ListCollection.');
-                C.currentCollection = C.listCollection;
+                C.log('App: init at ListCollection.')
+                C.currentCollection = C.listCollection
                 break;
-
+        
             case C.states.TODO_COLLECTION_VIEW:
-                C.log('App: init at TodoCollection with order: ' + state.order);
+                case.log(`App: init at TodoCollection with order: ${state.order}`)
                 while (i--) {
                     if (lists[i].order === state.order) {
-                        C.currentCollection = new C.TodoCollection(lists[i]);
-                        break;
+                        C.currentCollection = new C.TodoCollection(lists[i])
+                        break
                     }
                 }
-                break;
+                break
 
             default:
-                C.log('App: init at ListCollection.');
-                C.currentCollection = C.listCollection;
-                break;
-
+                C.log('App: init at ListCollection.')
+                C.currentCollection = C.listCollection
+                break
         }
-
-        C.currentCollection.load(0, true); // passing in (position:0) and (noAnimation:true)
-
-        if (!C.listCollection.initiated) {
-            // If we started with a TodoCollection, load ListCollection and position it for pulldown
-            C.listCollection.positionForPulldown();
-            C.listCollection.load();
-        } else {
-            // otherwise, load the last used todoCollection
-            C.lastTodoCollection = new C.TodoCollection(lists[state.lastTodoCollection || 0]);
-            C.lastTodoCollection.load(C.client.height + C.ITEM_HEIGHT, true);
-            C.lastTodoCollection.positionForPullUp();
-        }
-
-    },
-
-    setCurrentCollection: function (col) {
-
-        var msg = 'Current collection set to: '
-        C.log(msg + (col.data.title ? 'TodoCollection <' + col.data.title + '>' : 'ListCollection'));
-
-        C.currentCollection = col;
-        var state = C.db.data.state;
-        state.view = col.stateType;
-        state.order = col.data.order;
-        C.db.save();
-
-    },
-
-    setLastTodoCollection: function (col) {
-
-        C.lastTodoCollection = col;
-        C.db.data.state.lastTodoCollection = col.data.order;
-        C.db.save();
-
     },
 
     log: function (msg) {
+        if (!this.deb) {
+            return
+        }
 
-        if (!this.debug) return;
+        // document.getElementById('log').textContent = msg
 
-        //$('#log').text(msg);
-
-        var time = Date.now() - C.start;
+        var time = Date.now() - C.start
         if (time < 1000) {
-            time = '[' + time + 'ms] ';
+            time = '[${time}ms] '
         } else {
-            time = '[' + (time / 1000).toFixed(2) + 's] ';
+            time = '[${(time / 1000).toFixed(2)}s] '
         }
-        msg = time + msg;
-        console.log(msg);
-
+        msg = time + msg
+        console.log(msg)
     },
-
-    raf: window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        function (callback) {
-            window.setTimeout(callback, 16);
-        }
-
-};
-
-// boot up on page load
-$(function () {
-    C.init();
-});
+}
