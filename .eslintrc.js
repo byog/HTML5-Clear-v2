8a989b52a144230d465d17917883d595a95fa832
lean <7764115+byog@users.noreply.github.com>
2020-06-07 20:22:56 +0800

    chore: eslint, prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
+    root: true,
     env: {
         browser: true,
         es2020: true,
     },
-    extends: 'eslint:recommended',
+    extends: ['airbnb', 'prettier'],
     parserOptions: {
         ecmaVersion: 11,
         sourceType: 'module',
